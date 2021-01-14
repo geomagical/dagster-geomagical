a770d4a6d03516ff6b6273451a275a809d8e2d31
Noah Kantrowitz <noah@coderanger.net>
2021-01-13 21:47:16 -0800

    🎨 Back out a previous change, instead just don't load anything in __init__ since it gets used in different contexts.

--- dagster_geomagical/__init__.py
+++
@@ -1,1 +0,0 @@
-from .definitions import celery_solid, celery_pipeline
--- setup.py
+++ setup.py
@@ -1,10 +1,4 @@
 from setuptools import setup, find_packages
 
 setup(name='dagster-geomagical',
-      packages=find_packages(),
-      install_requires=[
-            'dagster',
-            'Celery',
-            'redis',
-            'google-cloud-storage',
-      ])
+      packages=find_packages())
